6db17121675e92f2c56a6f48441621ee0d64e5e6
groMMaster <81671187+groMMaster@users.noreply.github.com>
2021-06-01 14:41:52 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,11 @@ namespace WindowsFormsApp4
 {
     public partial class Form1 : Form
     {
+        int NumberLevel = 0;
         public Form1()
         {
             InitializeComponent();
+            CreateLevels();
 
             string[] myFruit = { "A", "B", "C", "D", "E", "F" };
             checkedListBox1.Items.AddRange(myFruit);
@@ -27,6 +29,8 @@ namespace WindowsFormsApp4
             button1.Enabled = false;
             button1.Text = "Продолжить";
             button1.Font = new Font("Arial Black", 24);
+
+            pictureBox1.Image = LevelList[NumberLevel].Condition;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -36,8 +40,8 @@ namespace WindowsFormsApp4
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            List<string> result = new List<string> { "A", "B", "C" };
-            List<string> checkingResponses = new List<string> {};
+            List<string> result = LevelList[NumberLevel].Answer;
+            List<string> checkingResponses = new List<string> { };
             foreach (object itemChecked in checkedListBox1.CheckedItems)
             {
                 checkingResponses.Add(itemChecked.ToString());
@@ -49,12 +53,25 @@ namespace WindowsFormsApp4
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            NumberLevel += 1;
+            pictureBox1.Image = LevelList[NumberLevel].Condition;
         }
-    }
 
-    public class Levels
-    {
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+        public struct Level
+        {
+            public Image Condition;
+            public List<string> Answer;
+        }
 
+        public static List<Level> LevelList = new List<Level> { };
+        public void CreateLevels()
+        {
+            LevelList.Add(new Level { Condition = Resource1.B1, Answer = new List<string> { "A", "B", "C" } });
+            LevelList.Add(new Level { Condition = Resource1.B2, Answer = new List<string> { "A", "B", "D" } });
+            LevelList.Add(new Level { Condition = Resource1.B3, Answer = new List<string> { "A", "B", "E" } });
+        }
     }
 }
