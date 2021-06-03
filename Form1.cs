3a1249659424fe84c9934dc80672273371c8620b
groMMaster <81671187+groMMaster@users.noreply.github.com>
2021-06-03 10:17:17 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,67 +12,68 @@ namespace WindowsFormsApp4
 {
     public partial class Form1 : Form
     {
-        int Level = 0;
-        Label label;
+        public int Level = 0;
         public Form1()
         {
             InitializeComponent();
             Levels.CreateLevels();
-
-            label = new Label
-            {
-                Location = new Point(450, 300),
-                Size = new Size(400, 250),
-                Text = Levels.LevelList[Level].Question
-            };
-
-            checkedListBox1.Items.AddRange(new string[] { "A", "B", "C", "D", "E", "F" });
-
-            checkedListBox1.CheckOnClick = true;
-            checkedListBox1.Font = new Font("Arial Black", 23);
-            checkedListBox1.BackColor = Color.Beige;
-            checkedListBox1.MultiColumn = true;
-
-            button1.Enabled = false;
-            button1.Text = "Продолжить";
-            button1.Font = new Font("Arial Black", 24);
-
-            pictureBox1.Image = Levels.LevelList[Level].Condition;
+            CreateTextСondition();
+            CreateResponseForm();
+            CreateJumpButton();
+            GraphicСondition.Image = Levels.LevelList[Level].Condition;
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        public void CreateJumpButton()
         {
+            JumpButton.Enabled = false;
+            JumpButton.Text = "Продолжить";
+            JumpButton.Font = new Font("Arial Black", 24);
         }
 
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        public void CreateTextСondition()
+        {
+            TextСondition.Location = new Point(476, 45);
+            TextСondition.Text = Levels.LevelList[Level].Question;
+            TextСondition.Font = new Font("Arial Black", 23);
+            TextСondition.MaximumSize = new Size(450, 0);
+            TextСondition.BackColor = Color.Beige;
+        }
+
+        public void CreateResponseForm()
+        {
+            ResponseForm.Items.AddRange(new string[] { "A", "B", "C", "D", "E", "F" });
+            ResponseForm.CheckOnClick = true;
+            ResponseForm.Font = new Font("Arial Black", 23);
+            ResponseForm.BackColor = Color.Beige;
+            ResponseForm.MultiColumn = true;
+        }
+
+        private void ResponseForm_SelectedIndexChanged(object sender, EventArgs e)
         {
             List<string> result = Levels.LevelList[Level].Answer;
             List<string> checkingResponses = new List<string> { };
-            foreach (object itemChecked in checkedListBox1.CheckedItems)
+            foreach (object itemChecked in ResponseForm.CheckedItems)
             {
                 checkingResponses.Add(itemChecked.ToString());
             }
 
             if (checkingResponses.SequenceEqual(result))
-                button1.Enabled = true;
+                JumpButton.Enabled = true;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void JumpButton_Click(object sender, EventArgs e)
         {
             Level += 1;
-            pictureBox1.Image = Levels.LevelList[Level].Condition;
-            button1.Enabled = false;
-            checkedListBox1.Items.Clear();
-            checkedListBox1.Items.AddRange(new string[] { "A", "B", "C", "D", "E", "F" });
-            label.Text = Levels.LevelList[Level].Question;
-        }
+            if (Level == Levels.LevelList.Count)
+            {
+                
+            }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
+            GraphicСondition.Image = Levels.LevelList[Level].Condition;
+            JumpButton.Enabled = false;
+            ResponseForm.Items.Clear();
+            ResponseForm.Items.AddRange(new string[] { "A", "B", "C", "D", "E", "F" });
+            TextСondition.Text = Levels.LevelList[Level].Question;
         }
     }
 
@@ -89,12 +90,26 @@ namespace WindowsFormsApp4
 
         public static void CreateLevels()
         {
-            LevelList.Add(new Level { Condition = Resource1.B1, Answer = new List<string> { "A", "B", "C" }, 
-                Question = "Укажите узлы, удаленные от данного на расстояние √5 и меньше"});
-            LevelList.Add(new Level { Condition = Resource1.B2, Answer = new List<string> { "A", "B", "D" }, 
-                Question = ""});
-            LevelList.Add(new Level { Condition = Resource1.B3, Answer = new List<string> { "A", "B", "E" }, 
-                Question = ""});
+            LevelList.Add(new Level
+            {
+                Condition = Resource1.L1,
+                Answer = new List<string> { "F" },
+                Question = "Укажите узел, симметричный данному"
+            });
+
+            LevelList.Add(new Level
+            {
+                Condition = Resource1.L2,
+                Answer = new List<string> { "B" },
+                Question = "Укажите узел, равноудаленный от данных"
+            });
+
+            LevelList.Add(new Level
+            {
+                Condition = Resource1.L3,
+                Answer = new List<string> { "A", "B", "C" },
+                Question = "Укажите узлы, удаленные от данного на расстояние √5 и меньше"
+            });
         }
     }
 }
